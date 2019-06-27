7a6a5ddf98bfa476e166998ea7336656449dcd57
GeorgeChenZJ <384916369@qq.com>
2019-06-27 17:08:51 +0800

    a

---
+++ precache-manifest.4556d0b7584c8b52c5cb244c28416c24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/draw-a-car-build/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bc70f5b3e493ee583b73",
+    "url": "/draw-a-car-build/static/js/runtime~main.bc70f5b3.js"
+  },
+  {
+    "revision": "5273e3918d480f6cbe34",
+    "url": "/draw-a-car-build/static/js/main.5273e391.chunk.js"
+  },
+  {
+    "revision": "ca913eae12694b3b0974",
+    "url": "/draw-a-car-build/static/js/1.ca913eae.chunk.js"
+  },
+  {
+    "revision": "5273e3918d480f6cbe34",
+    "url": "/draw-a-car-build/static/css/main.01bd4a31.chunk.css"
+  },
+  {
+    "revision": "6d7b4e2b95860ab939e0440d30b03fe6",
+    "url": "/draw-a-car-build/index.html"
+  }
+];
